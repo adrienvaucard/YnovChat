e09f885a4bda5b6f1a493fabc924a72cc8d269c4
Adrien <adrien.vaucard@gmail.com>
2022-01-06 13:40:02 +0100

    feat(details): Add message details

--- app/src/main/java/com/example/myawesomeapp/fragment/MessageDetailFragment.java
+++ app/src/main/java/com/example/myawesomeapp/fragment/MessageDetailFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -13,20 +14,30 @@ import android.widget.Toast;
 
 import com.example.myawesomeapp.Message;
 import com.example.myawesomeapp.R;
+import com.example.myawesomeapp.databinding.FragmentMessageDetailBinding;
+import com.example.myawesomeapp.databinding.RowLayoutMessageBinding;
 
 public class MessageDetailFragment extends Fragment {
+    FragmentMessageDetailBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_message_detail, container, false);
+        binding = DataBindingUtil.inflate(
+                LayoutInflater.from(getContext()),
+                R.layout.fragment_message_detail,
+                container,
+                false
+        );
+
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Message msg = MessageDetailFragmentArgs.fromBundle(getArguments()).getMessage();
-        Toast.makeText(view.getContext(), msg.getContent(), Toast.LENGTH_SHORT).show();
+        binding.setMessage(msg);
+        //Toast.makeText(view.getContext(), msg.getContent(), Toast.LENGTH_SHORT).show();
     }
 }

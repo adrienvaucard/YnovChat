6087adf7428989c24cfbc53ebc5178dceef1e9bd
Adrien <adrien.vaucard@gmail.com>
2022-01-06 12:21:40 +0100

    fix(msg): Fix msg show on view holder

--- app/src/main/java/com/example/myawesomeapp/adapter/MessageAdapter.java
+++ app/src/main/java/com/example/myawesomeapp/adapter/MessageAdapter.java
@@ -43,6 +43,7 @@ public class MessageAdapter extends RecyclerView.Adapter<MessageHolder> {
                 (view) -> Navigation.findNavController(holder.itemView)
                 .navigate(action)
         );
+        holder.binding.setMessage(msg);
     }
 
     @Override
--- app/src/main/java/com/example/myawesomeapp/fragment/MessageDetailFragment.java
+++ app/src/main/java/com/example/myawesomeapp/fragment/MessageDetailFragment.java
@@ -2,12 +2,16 @@ package com.example.myawesomeapp.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.myawesomeapp.Message;
 import com.example.myawesomeapp.R;
 
 public class MessageDetailFragment extends Fragment {
@@ -17,5 +21,12 @@ public class MessageDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_message_detail, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Message msg = MessageDetailFragmentArgs.fromBundle(getArguments()).getMessage();
+        Toast.makeText(view.getContext(), msg.getContent(), Toast.LENGTH_SHORT).show();
     }
 }

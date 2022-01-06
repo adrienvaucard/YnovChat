ce8bc3cc15b20eaf89ad341f0384ce9cae6867b8
Adrien <adrien.vaucard@gmail.com>
2022-01-06 15:38:56 +0100

    feat(res): fix custom svg

--- app/src/main/java/com/example/myawesomeapp/fragment/MessageListFragment.java
+++ app/src/main/java/com/example/myawesomeapp/fragment/MessageListFragment.java
@@ -87,6 +87,10 @@ public class MessageListFragment extends Fragment {
         });
     }
 
+    private void sendMessage() {
+
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

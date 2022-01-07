753eb2d3063568b0d8df4bfb5d4f5a78462302a8
Adrien <adrien.vaucard@gmail.com>
2022-01-07 10:16:31 +0100

    feat(layout): fix height of messages

--- app/src/main/java/com/example/myawesomeapp/activity/MainActivity.java
+++ app/src/main/java/com/example/myawesomeapp/activity/MainActivity.java
@@ -1,20 +1,37 @@
 package com.example.myawesomeapp.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.myawesomeapp.R;
+import com.example.myawesomeapp.fragment.MessageListFragment;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        // Intent loginPage = new Intent(MainActivity.this, LoginActivity.class);
-        // startActivity(loginPage);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list_msg, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.item_refresh) {
+            Toast.makeText(this, "Chargement des nouveaux messages", Toast.LENGTH_SHORT).show();
+            MessageListFragment mlf = (MessageListFragment) getSupportFragmentManager().findFragmentById(R.id.messageListFragment);
+            //mlf.refresh();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
